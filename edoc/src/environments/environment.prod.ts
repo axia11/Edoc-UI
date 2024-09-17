89ba272f30a7c2b93887a7a35f8c301a221be27d
axia11 <115462289+axia11@users.noreply.github.com>
2024-09-17 12:40:43 +0530

    Update environment.prod.ts

--- edoc/src/environments/environment.prod.ts
+++ edoc/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://node.edocx.ai/Axia4', // backend connection for db
+  apiUrl: 'https://node.edocx.ai/edoc', // backend connection for db
   redirectUrl: 'https://accounts.blunetworld.org/', // click sign in button redirect to lucy login
   baseUrl: localStorage.dynamicUrl || '', // sign in - sign up button page redirection
   lucyApiUrl: 'https://java.edocx.ai' // only for signout - in dashboard file (inside lib)
